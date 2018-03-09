03e52eac1438be4a5d04f42df9b3b33e16563a43
Jordan Deam <jordan.deam@gmail.com>
2018-03-09 10:39:31 -0800

    Combined Message and MessageList components into one file

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Message from './Message';
 
 const MessageList = ({ messages, selectMessage, starMessage }) => (
   <div>
@@ -9,6 +8,36 @@ const MessageList = ({ messages, selectMessage, starMessage }) => (
       selectMessage={ selectMessage(i) }
       starMessage={ starMessage(i) }
     />)}
+  </div>
+);
+
+const Message = ({ message, selectMessage, starMessage }) => (
+  <div className={ `row message ${ message.read?"read":"unread" } ${ message.selected?"selected":"" }`}>
+    <div className="col-xs-1">
+      <div className="row">
+        <div className="col-xs-2">
+          <input
+            type="checkbox"
+            checked={ message.selected?"checked":"" }
+            onChange={ selectMessage }
+           />
+        </div>
+        <div className="col-xs-2">
+          <i
+            className={ message.starred?"star fa fa-star":"star fa fa-star-o"}
+            onClick={ starMessage }
+            ></i>
+        </div>
+      </div>
+    </div>
+    <div className="col-xs-11">
+      { message.labels.map((label, i) => (
+        <span key={ i } className="label label-warning">{ label }</span>
+      )) }
+      <a href="#">
+        { message.subject }
+      </a>
+    </div>
   </div>
 );
 
