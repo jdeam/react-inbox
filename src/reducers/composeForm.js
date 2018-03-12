fccbfdf092338abb7b11c77786f8846eeeaacff4
Jordan Deam <jordan.deam@gmail.com>
2018-03-11 18:57:57 -0700

    Wrote message and composeForm reducers

--- src/reducers/composeForm.js
+++ src/reducers/composeForm.js
@@ -5,23 +5,46 @@ import {
   CLEARED_COMPOSE_FORM
 } from '../actions';
 
-function composeForm(state = {}, action) {
+const initialFormState = { display: false, content: { subject: '', body: '' } };
+
+function composeForm(state = initialFormState, action) {
   switch(action.type) {
     case TOGGLED_COMPOSE_FORM:
-      return {
-
-      };
+      if (action.toggle) {
+        return {
+          ...this.state,
+          display: true
+        };
+      } else {
+        return {
+          ...this.state,
+          display: false,
+          content: { subject: '', body: ''}
+        }
+      }
     case UPDATED_FORM_SUBJECT:
       return {
-
+        ...this.state,
+        content: {
+          ...this.state.content,
+          subject: action.subject
+        }
       };
     case UPDATED_FORM_BODY:
       return {
-
+        ...this.state,
+        content: {
+          ...this.state.content,
+          body: action.body
+        }
       };
     case CLEARED_COMPOSE_FORM:
       return {
-
+        ...this.state,
+        content: {
+          subject: '',
+          body: ''
+        }
       };
     default:
       return state;
--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -9,7 +9,7 @@ import {
   REMOVED_LABEL,
   DELETED_MESSAGE,
   MESSAGE_CREATED
-} from '../actions'
+} from '../actions';
 
 function messages(state = { messages: [] }, action) {
   switch (action.type) {
@@ -19,36 +19,88 @@ function messages(state = { messages: [] }, action) {
         messages: action.messages
       };
     case SELECTED_SINGLE_MESSAGE:
+      const newState = [ ...this.state.messages ];
+      if (newState[action.id].selected) delete newState[action.id].selected;
+      else newState[action.id].selected = true;
       return {
-
+        ...state,
+        messages: newState
       };
     case STARRED_SINGLE_MESSAGE:
+      const newState = [ ...this.state.messages ];
+      if (newState[action.id].starred) newState[action.id].starred = false;
+      else newState[action.id].starred = true;
       return {
-
+        ...state,
+        messages: newState
       };
     case SELECTED_ALL_MESSAGES:
+      let newState;
+      if (action.selected) {
+        newState = this.state.messages.map(message => {
+          message.selected = true;
+          return message;
+        });
+      } else {
+        newState = this.state.messages.map(message => {
+          if (message.selected) delete message.selected;
+          return message;
+        });
+      }
       return {
-
+        ...state,
+        messages: newState
       };
     case MARKED_READ:
+      const newState = this.state.messages.map(message => {
+        if (action.ids.includes(message.id)) message.read = true;
+        return message;
+      });
       return {
-
+        ...this.state,
+        messages: newState
       };
     case MARKED_UNREAD:
+      const newState = this.state.messages.map(message => {
+        if (action.ids.includes(message.id)) message.read = false;
+        return message;
+      });
       return {
-
+        ...this.state,
+        messages: newState
       };
     case APPLIED_LABEL:
+      const ids = action.apply.ids;
+      const label = action.apply.label;
+      const newState = this.state.messages.map(message => {
+        if (ids.includes(message.id) && !message.labels.includes(label)) {
+          message.labels.push(label);
+        }
+        return message;
+      });
       return {
-
+        ...this.state,
+        messages: newState
       };
     case REMOVED_LABEL:
+      const ids = action.remove.ids;
+      const label = action.remove.label;
+      const newState = this.state.messages.map(message => {
+        if (ids.includes(message.id)) {
+          message.labels = message.labels.filter(el => el !== label);
+        }
+      });
       return {
-
+        ...this.state,
+        messages: newState
       };
     case DELETED_MESSAGE:
+      const newState = this.state.messages.filter(message => {
+        return !action.ids.includes(message.id);
+      });
       return {
-
+        ...this.state,
+        messages: newState
       };
     case MESSAGE_CREATED:
       return {
